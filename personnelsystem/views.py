3c9ada6f949ba6f2d6ff2a762697e6e3c718b6f7
chinedu amadi-ndukwe <chinedua@umich.edu>
2019-04-26 08:45:47 -0400

    first commit

---
+++ personnelsystem/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    from django.http import HttpResponse
+    return HttpResponse("welcome to the personnel page")
+

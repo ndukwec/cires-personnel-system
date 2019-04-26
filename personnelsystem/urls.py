2179de66ddaaff187dfebe207cd336b38f2cdaac
chinedu amadi-ndukwe <chinedua@umich.edu>
2019-04-26 11:01:57 -0400

    updated model, added listview, updated base.html, views.py and urls.py

--- personnelsystem/urls.py
+++ personnelsystem/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, PersonnelListView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('personnel-list/', PersonnelListView.as_view(), name='personnel-list'),
 ]
+
